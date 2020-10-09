f2d5fa064442e127a2b20d01f590a5ce1f4c4b64
unknown <viniciusbeloti7@gmail.com>
2020-10-09 16:29:25 -0300

    chore: add ChatHeader and ChatRight

--- src/components/ChatHeader/index.tsx
+++ src/components/ChatHeader/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
+import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
 
 import styles from './ChatHeader.module.css';
 
 const ChatHeader = () => (
   <div className={styles.chatHeader}>
     <h2>ChatHeader</h2>
+
+    <div className={styles.chatHeader__left}>
+      <h3>
+        <span className={styles.chatHeader__hash}>#</span>
+        Nome do Canal
+      </h3>
+    </div>
+
+    <div className={styles.chatHeader__right}>
+      <NotificationsIcon />
+      <EditLocationRoundedIcon />
+      <PeopleAltRoundedIcon />
+    </div>
   </div>
 );
 
