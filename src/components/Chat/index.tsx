664eabc9236cf86d517ee4fc131eb37b3df43abc
unknown <viniciusbeloti7@gmail.com>
2020-10-09 16:26:16 -0300

    chore: add ChatHeader to Chat

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ChatHeader from '../ChatHeader';
 
 import styles from './Chat.module.css';
 
 const Chat = () => (
   <div className={styles.chat}>
     <h2>Chat</h2>
+    <ChatHeader />
   </div>
 );
 
